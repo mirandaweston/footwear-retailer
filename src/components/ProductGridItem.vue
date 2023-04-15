d17e14f92c46cd79637513d8b8a39d73097c66fa
mirandaweston <mweston105@yahoo.co.uk>
2023-04-15 16:14:57 +0100

    Adding ProductGridItem component - tests passing

---
+++ src/components/ProductGridItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="product">
+    <img :src="image" alt="Product Image" />
+    <h3>{{ name }}</h3>
+    <p>{{ brand }}</p>
+    <p>{{ price }}</p>
+    <p>{{ rank }}</p>
+    <p v-if="inStock">In stock</p>
+    <p v-else>Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    id: Number,
+    name: String,
+    brand: String,
+    price: Number,
+    inStock: Boolean,
+    image: String,
+    rank: Number,
+  },
+};
+</script>
+
+<style scoped>
+.product {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  width: 250px;
+  display: inline-block;
+  vertical-align: top;
+  text-align: center;
+}
+
+.product img {
+  max-width: 100%;
+}
+</style>
